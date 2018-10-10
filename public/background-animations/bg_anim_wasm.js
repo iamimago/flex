0aebe796a019b3bb0d2cb16104da7b5c9c926a91
Lorentz Kinde <lorkin-4@student.ltu.se>
2018-10-10 13:20:46 +0200

    memory mapped hit detection system

--- public/background-animations/bg_anim_wasm.js
+++ public/background-animations/bg_anim_wasm.js
@@ -30,42 +30,34 @@
 
     let offset = 0,
         tog,
-        move_factor = 5;
+        move_factor = 1,
+        blocked_arr = [],
+        menu_changed = 0;
 
     function animate() {
-        let bbox = document.getElementsByClassName("sq_menu-item"), intersect_x, move_y;
 
+        let bbox = document.getElementsByClassName("sq_menu-item"), blocked_x, move_y;
         for (let i = 0; i < NUM_PARTICLES; i++) {
             p = list[i];
             //next_step_y = p.y + Math.sin(i + offset) * move_factor;
-            next_step_x = p.x + Math.cos(i + offset) * move_factor;
-            intersect_x = false;
+            next_step_x = p.x + Math.cos(offset) * move_factor;
+            blocked_x = false;
 
-            for(let i = 0; i < bbox.length; i++){
-                let b_bot = bbox[i].getBoundingClientRect().bottom,
-                    b_top = bbox[i].getBoundingClientRect().top,
-                    b_left = bbox[i].getBoundingClientRect().left,
-                    b_right = bbox[i].getBoundingClientRect().right;
-
-                if(next_step_x < b_right && next_step_x > b_left && p.y < b_bot && p.y > b_top && !intersect_x){
-                    //It is intersecting
-                    if(next_step_x < p.x) next_step_x = b_right;
-                    if(next_step_x > p.x) next_step_x = b_left;
-                    intersect_x = true;
-                }
-
-                if(next_step_x < 0){
-                    next_step_x = w;
-                    intersect_x = true;
-                }
-
-                if(next_step_x > w){
-                    next_step_x = 0;
-                    intersect_x = true;
-                }
+            if(blocked_arr[~~next_step_x * ~~p.y] == 1){
+                blocked_x = true;
             }
 
-            if(!intersect_x) p.x = next_step_x;
+            if(next_step_x < 0){
+                next_step_x = w;
+                blocked_x = true;
+            }
+
+            if(next_step_x > w){
+                next_step_x = 0;
+                blocked_x = true;
+            }
+
+            if(!blocked_x) p.x = next_step_x;
         }
         offset += 0.01;
 
@@ -92,7 +84,6 @@
                 p = list[i];
                 b[n = (~~p.x + (~~p.y * w)) * 4] = b[n + 1] = b[n + 2] = COLOR, b[n + 3] = 255;
             }
-
             
             ctx.putImageData(a, 0, 0);
         }else{
@@ -118,13 +109,49 @@
                 count++;
             }
         }
+
+        let bbox = document.getElementsByClassName("sq_menu-item"), bounds = [];
+        //Construct the blocked array used for collision detection        
+        for(let i = 0; i < bbox.length; i++){            
+            bounds.push({
+                    b_bot: bbox[i].getBoundingClientRect().bottom,
+                    b_top: bbox[i].getBoundingClientRect().top,
+                    b_left: bbox[i].getBoundingClientRect().left,
+                    b_right: bbox[i].getBoundingClientRect().right});
+        }
+
+        console.log(bounds);
+        let set_blocked_flag = 0;
+        for(let p_x = 0; p_x < w; p_x++){                
+            for(let p_y = 0; p_y < h; p_y++){
+                set_blocked_flag = 0;
+                for(let i = 0; i < bounds.length; i++){
+                    if(p_x > bounds[i].b_left && p_x < bounds[i].b_right && p_y > bounds[i].b_top && p_y <bounds[i].b_bot){
+                        set_blocked_flag = 1;
+                    }
+                }
+                if(set_blocked_flag){
+                    blocked_arr.push(1);
+                }else{
+                    blocked_arr.push(0);       
+                }          
+                
+            }
+        }
+        console.log({blocked_arr});
+        
+        
+
         //Easymode: (<script src="/web-assembly/test.js"></script> in index.html
+        //Todo: Feed the blocked array into the memory of wasm and then check detection through wasm, see if it's faster.
         
         Module.onRuntimeInitialized = function() {
             console.log(Module._fib())
         }
 
-        step();
+        
+
+        //step();
         //fpsCounter();
     }
 
